57fcb026f0ed4ae09c15303b78fec51e05046720
walkergitonga <walkergitonga@gmail.com>
2016-04-05 22:46:34 +0300

    Added views to create/edit inquiry inside the portal.

--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
     url(r'^$', views.inquiry_list, name='inquiry_list'),
     url(r'^inquiry/(?P<pk>\d+)/$', views.inquiry_detail, name='inquiry_detail'),
+    url(r'^inquiry/new/$', views.inquiry_new, name='inquiry_new'),
+    url(r'^inquiry/(?P<pk>\d+)/edit/$', views.inquiry_edit, name='inquiry_edit'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Inquiry
+from .forms import InquiryForm
 
 # Create your views here.
 def inquiry_list(request):
@@ -10,4 +11,30 @@ def inquiry_list(request):
 def inquiry_detail(request, pk):
     inquiry = get_object_or_404(Inquiry, pk=pk)
     return render(request, 'contact/inquiry_detail.html', {'inquiry': inquiry})
+
+def inquiry_new(request):
+    if request.method == "POST":
+        form = InquiryForm(request.POST)
+        if form.is_valid():
+            inquiry = form.save(commit=False)
+            inquiry.date = timezone.now()
+            inquiry.save()
+            return redirect('inquiry_detail', pk=inquiry.pk)
+    else:
+        form = InquiryForm()
+    return render(request, 'contact/inquiry_edit.html', {'form': form})
+
+def inquiry_edit(request, pk):
+    inquiry = get_object_or_404(Inquiry, pk=pk)
+    if request.method == "POST":
+        form = InquiryForm(request.POST, instance=inquiry)
+        if form.is_valid():
+            inquiry = form.save(commit=False)
+            inquiry.date = timezone.now()
+            inquiry.save()
+            return redirect('inquiry_detail', pk=inquiry.pk)
+    else:
+        form = InquiryForm(instance=inquiry)
+    return render(request, 'contact/inquiry_edit.html', {'form': form})
+
 
