42e4a91eeec58b6d1943529432c1e23b982a66df
walkergitonga <walkergitonga@gmail.com>
2016-04-05 21:43:48 +0300

    Added view and template for detailed inquiry as well as CSS for the site.

--- contact/urls.py
+++ contact/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.inquiry_list, name='inquiry_list'),
+    url(r'^inquiry/(?P<pk>\d+)/$', views.inquiry_detail, name='inquiry_detail'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Inquiry
 
@@ -6,3 +6,8 @@ from .models import Inquiry
 def inquiry_list(request):
     inquiries = Inquiry.objects.filter(date__lte=timezone.now()).order_by('date')
     return render(request, 'contact/inquiry_list.html', {'inquiries': inquiries})
+
+def inquiry_detail(request, pk):
+    inquiry = get_object_or_404(Inquiry, pk=pk)
+    return render(request, 'contact/inquiry_detail.html', {'inquiry': inquiry})
+
